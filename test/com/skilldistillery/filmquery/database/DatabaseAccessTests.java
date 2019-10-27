3b62c1154e346782f8af5f71396822e5331c76c7
Zak Saylors <zsaylors11@gmail.com>
2019-10-26 20:19:40 -0600

    Some test cases added for practice.  Unfortunately, they are not as useful after the program has been written and the mistakes made have already been fixed.

--- test/com/skilldistillery/filmquery/database/DatabaseAccessTests.java
+++ test/com/skilldistillery/filmquery/database/DatabaseAccessTests.java
@@ -2,6 +2,8 @@ package com.skilldistillery.filmquery.database;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,5 +27,47 @@ class DatabaseAccessTests {
   void test_getFilmById_with_invalid_id_returns_null() {
     Film f = db.findFilmById(-42);
     assertNull(f);
+  }
+  
+  @Test
+  void test_getFilmById_with_no_id_returns_null() {
+    Film f = db.findFilmById(0);
+    assertNull(f);
+  }
+  
+  @Test
+  void test_getFilmById_with_1() {
+    Film f = db.findFilmById(1);
+    assertEquals("PG", f.getRating());
+  }
+  
+  @Test
+  void test_getFilmById_with_releaseYear() {
+	    Film f = db.findFilmById(1);
+	    assertEquals(1993, f.getReleaseYear());
+	  }
+  
+  @Test
+  void test_getFilmById_with_non_existant_Id() {
+	    Film f = db.findFilmById(1001);
+	    assertNull(f);
+	  }
+  
+  @Test
+  void test_findFilmBySearch_with_non_existant_Id() {
+	    List<Film> lf = db.findFilmByKeyword("mad cow");
+	    assertEquals(74, lf.size());
+	  }
+  
+  @Test
+  void test_findFilmBySearch_with_non_existant_Id_test2() {
+	  List<Film> lf = db.findFilmByKeyword("Monkey who must Defeat");
+	  assertEquals(1, lf.size());
+  }
+  
+  @Test
+  void test_findFilmBySearch_with_non_existant_with_no_input() {
+	  List<Film> lf = db.findFilmByKeyword("");
+	  assertEquals(1000, lf.size());
   }
 }
